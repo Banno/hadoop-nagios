181d4ecfe2cc2bfd114e51c4cbfff285dbec9155
Daniel Lockard <danny.lockard@t8webware.com>
2012-01-09 10:32:05 -0600

    Added Warn / Crit, performance data, and exit codes

--- hadoop_jmx.rb
+++ hadoop_jmx.rb
@@ -25,12 +25,24 @@ end
 opts = Trollop::options do
 	opt :address, "Hadoop IP or address", :default => "127.0.0.1", :short => '-a'
 	opt :port, "Hadoop webserver port w/ JMX enabled", :default => "50070", :short => '-p'
+	opt :warning, "Percentage to warn on", :default => "85", :short => '-w'
+	opt :critical, "Percentage to crit on", :default => "95", :short => '-c'
 	opt :memory, "Whether or not to display percentage of used heap memory", :short => '-m'
 end
 
 $hadoop_json = load_hadoop_json(opts[:address],opts[:port])
 if opts[:memory]
-		puts check_memory
+	memory_used = check_memory
+	if (memory_used >= opts[:critical].to_f)
+			puts memory_used.to_s + "% of total heap used. CRITICAL! | percent_memory=" + memory_used.to_s
+		exit 2
+	elsif (memory_used >= opts[:warning].to_f)
+		puts memory_used.to_s + "% of total heap used. Warning! | percent_memory=" + memory_used.to_s
+		exit 1
+	else
+		puts memory_used.to_s + "% of total heap used. Everything looks good! | percent_memory=" + memory_used.to_s
+		exit 0
+	end
 end
 
 
