bc14a6ab817808cbfbd4ec08809bd6033bab2074
Daniel Lockard <danny.lockard@t8webware.com>
2012-01-16 14:37:45 -0600

    Adding a flume node pid check

---
+++ check_flume_node.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+is_running=0
+flume_node_pid=0
+
+if [ -e /var/run/flume-*-node.pid ]; then
+	$flume_node_pid=`cat /var/run/flume-*-node.pid`
+else
+	echo "Flume pid file not found!"
+	exit 3
+fi
+
+if [ $flume_node_pid != 0 ]; then
+	lines=`ps -p $flume_node_pid | wc -l`
+else
+	echo "Flume node not running!"
+	exit 3
+fi
+
+if [ $lines >= 1 ]; then 
+	echo "Flume node is running!"
+	exit 0
+else 
+	echo "Flume node is not running or pid file not found!"
+	exit 3
+fi
+
