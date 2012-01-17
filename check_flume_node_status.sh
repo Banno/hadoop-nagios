cbc6d9fce7891bf9cc5aea726133e5b5b5f9f763
Daniel Lockard <danny.lockard@t8webware.com>
2012-01-17 11:27:07 -0600

    Added more error checking and a Exit Code 3 status

--- check_flume_node_status.sh
+++ check_flume_node_status.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-flume_status=`echo "getnodestatus" | flume shell -c apps.banno.com 2>&1 | tail -n 2  |sed 's/\t//g' | awk '{print $3}' | sed 's/\n//g' | head -n 1`
+if [ "$1" == "" ]; then
+	echo "Hostname undefined"
+	echo "Usage: $0 host.name.here"
+	exit 3
+fi
+
+flume_status=`echo "getnodestatus" | flume shell -c $1 2>&1 | tail -n 2  |sed 's/\t//g' | awk '{print $3}' | sed 's/\n//g' | head -n 1`
 
 
 echo "Flume Status: $flume_status"
 if [ "$flume_status" == "ERROR" ]; then
 	exit 2
-else
+elif [ "$flume_status" == "ACTIVE" ]; then
 	exit 0
+else 
+	exit 3
 fi
